eb4e7934a6416f07c82304d1617136fd39309728
Jacek Kawalec <jokerchocolate@gmail.com>
2020-10-08 09:27:37 +0200

    file upload working

---
+++ videobeat-backend/app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request
+from flask_cors import CORS, cross_origin
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+cors = CORS(app, resources={r'/*': {"origins": '*'}})
+app.config['CORS_HEADER'] = 'Content-Type'
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+@cross_origin(origin='*', allow_headers=['Content-Type'])
+def index():
+    return 'Hello world'
+    
+@app.route('/uploadOne', methods = ['GET', 'POST'])
+@cross_origin(origin='*',allow_headers=['Content-Type'])
+def upload_file1():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return 'file uploaded succesfully'
+
+@app.route('/uploadTwo', methods = ['GET', 'POST'])
+@cross_origin(allow_headers=['Content-Type'])
+def upload_file2():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return 'file uploaded succesfully'
+
+if __name__ =="__main__":
+    print("server is running")
+    app.run()

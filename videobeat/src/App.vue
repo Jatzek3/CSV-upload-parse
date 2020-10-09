f90121de5e5249936ffc95e882cbbd1c001fe326
Jacek Kawalec <jokerchocolate@gmail.com>
2020-10-09 11:03:11 +0200

    frontend part

---
+++ videobeat/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <header-comp />
+    <upload />
+    <upload-second />
+  </div>
+</template>
+
+<script>
+import Upload from "./Upload.vue";
+import HeaderComp from "./HeaderComp.vue";
+import UploadSecond from "./uploadSecond";
+
+export default { 
+  
+  name: "App",
+  components: { Upload, HeaderComp, UploadSecond },
+};
+</script>
+
+<style>
+.app {
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 5em;
+}
+</style>
---
+++ videobeat/src/Upload.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="sendFile" enctype="multipart/form-data" class="form">
+    <label for="file" class="label">Upload file</label>
+    <input type="file" ref="file" @change="selectFile" />
+    <button>Send</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Upload",
+  data() {
+    return {
+      file: "",
+    };
+  },
+  methods: {
+    selectFile() {
+      this.file = this.$refs.file.files[0];
+    },
+    async sendFile() {
+      const formData = new FormData();
+      formData.append("file", this.file);
+      try {
+        await axios.post("http://localhost:5000/uploadOne", formData);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>

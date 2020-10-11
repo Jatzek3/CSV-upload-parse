9ec604ce74be50019405adfb77578ee257ff3a15
Jacek Kawalec <jokerchocolate@gmail.com>
2020-10-11 09:54:53 +0200

    fixed delimiter/csv problems

--- videobeat-backend/pars.py
+++ videobeat-backend/pars.py
@@ -11,7 +11,7 @@ def mainflow():
         import csv
         file =[]
         with open(filename, 'r') as second_file:
-            csv_reader2 = csv.DictReader(second_file,delimiter=';')
+            csv_reader2 = csv.DictReader(second_file,delimiter=',')
             for line in csv_reader2:
                 file.append(line)
         return file
@@ -22,26 +22,24 @@ def mainflow():
     first_file = createDict('client_request.csv')
     second_file = createDict('adblock.csv')
     # sorting client request by values Descending
-    first_file.sort(
-        key=lambda add: int(add['Budget']) / int(add['Duration']),
-        reverse=True),
+    first_file.sort(key=lambda add: int(add['Budget']) / int(add['Duration']),reverse=True),
 
 
     # Count the whole ammount of time in client requests and adds
-    # throw out the least profittable
+    # throw out the least profitable
     adds_time = 0
     adblock_time = 0
     for i in first_file:
         adds_time += int(i['Duration'])
     for i in second_file:
-         adblock_time += int(i['Duratation'])
+         adblock_time += int(i['Duration'])
     while adds_time > adblock_time:
         adds_time = 0
         adblock_time = 0
         for i in first_file:
             adds_time += int(i['Duration'])
         for i in second_file:
-            adblock_time += int(i['Duratation'])
+            adblock_time += int(i['Duration'])
         first_file.pop()
 
     print(adds_time)

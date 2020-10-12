aaf50f5d8b738a7a84c76d406c0d59cd7d319855
Jatzek3 <48227367+Jatzek3@users.noreply.github.com>
2020-10-12 12:19:09 +0200

    Update HeaderComp.vue

--- videobeat/src/HeaderComp.vue
+++ videobeat/src/HeaderComp.vue
@@ -1,19 +1,6 @@
 <template>
   <header>
     <h1>VideoBeat Vue-Flask App</h1>
-    <p>
-      Even though Vue 3 has stable release i decided to work with Vue 2, becouse
-      the main project is using Vue 2.
-    </p>
-    <p>
-      Flask was quite obvious choice for backend since creating rest API with
-      flask is very easy. Flask is also very compact comparing to full featur
-      framework like Django. Such a small project dont require using such a
-      complex tool + DRF. There was also other framework which I considered
-      aspecially FastAPI which i was recently very intrested, but its relatively
-      new framework and I wasn't sure if I will be able find quickly apropriate
-      learning materials/tutorials.
-    </p>
   </header>
 </template>
 
